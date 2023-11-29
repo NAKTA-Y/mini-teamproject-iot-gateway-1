940b2b9996d8eeaec84e6955c82bc4b7bb6990a8
pass0210 <pass0210@gmail.com>
2023-11-29 17:05:56 +0900

    Refactor : null 체크 수정

--- src/main/java/com/nhnacademy/mqtt/checker/NullValueChecker.java
+++ src/main/java/com/nhnacademy/mqtt/checker/NullValueChecker.java
@@ -6,6 +6,12 @@ public class NullValueChecker implements Checker {
 
     @Override
     public boolean check(JSONObject jsonObject) {
-        return jsonObject.getJSONObject("payload").get("value") != null;
+        System.out.println(jsonObject.getJSONObject("payload").get("value").getClass());
+        return jsonObject.getJSONObject("payload").get("value") != JSONObject.NULL;
+    }
+
+    public static void main(String[] args) {
+        JSONObject object = new JSONObject("{payload : {time : 2017, value : null}}");
+        System.out.println(new NullValueChecker().check(object));
     }
 }
