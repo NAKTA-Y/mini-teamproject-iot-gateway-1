0d98f38a7ed9658c2f295a7839ae0acd17abdf74
pass0210 <pass0210@gmail.com>
2023-11-29 17:14:29 +0900

    Refactor : 필요 없는 출력 제거

--- src/main/java/com/nhnacademy/mqtt/checker/NullValueChecker.java
+++ src/main/java/com/nhnacademy/mqtt/checker/NullValueChecker.java
@@ -6,7 +6,6 @@ public class NullValueChecker implements Checker {
 
     @Override
     public boolean check(JSONObject jsonObject) {
-        System.out.println(jsonObject.getJSONObject("payload").get("value").getClass());
         return jsonObject.getJSONObject("payload").get("value") != JSONObject.NULL;
     }
 }
