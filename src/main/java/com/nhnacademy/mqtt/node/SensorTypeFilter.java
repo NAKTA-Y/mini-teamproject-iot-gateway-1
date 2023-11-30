784a274a7a5a4ec2edff3d1bbc8dff1e39136efc
kimjaehyeok <rlawogur329@mail.ulsan.ac.kr>
2023-11-30 13:40:14 +0900

    sensor type filter, value checker 작성 완료

--- src/main/java/com/nhnacademy/mqtt/node/SensorTypeFilter.java
+++ src/main/java/com/nhnacademy/mqtt/node/SensorTypeFilter.java
@@ -1,16 +1,16 @@
 package com.nhnacademy.mqtt.node;
 
 import com.nhnacademy.mqtt.checker.Checker;
-import com.nhnacademy.mqtt.checker.ValueChecker;
 import com.nhnacademy.mqtt.message.Message;
 import org.json.JSONObject;
 
-public class AppNameFilter extends InputOutputNode {
-    private final Checker valueChecker;
+public class SensorTypeFilter extends InputOutputNode {
+    private final Checker sensorTypeChecker;
 
-    public AppNameFilter(int inputCount, int outputCount, ValueChecker valueChecker) {
+    // 생성자 입력 개수, 출력 개수, 센서타입체커
+    public SensorTypeFilter(int inputCount, int outputCount, Checker sensorTypChecker) {
         super(inputCount, outputCount);
-        this.valueChecker = valueChecker;
+        this.sensorTypeChecker = sensorTypChecker;
     }
 
     @Override
@@ -20,11 +20,13 @@ public class AppNameFilter extends InputOutputNode {
                 Message<JSONObject> receiveMessage = tryGetMessage();
                 JSONObject jsonObject = receiveMessage.getPayload();
 
-                if (valueChecker.check(jsonObject)) {
-                    Message<JSONObject> sendMessage = new Message<JSONObject>(jsonObject);
+                // 센서타입체커로 메시지 필드 체크
+                if (sensorTypeChecker.check(jsonObject)) {
+                    Message<JSONObject> sendMessage = new Message<>(jsonObject);
                     output(0, sendMessage);
-                }
 
+                }
+                // interruptedexception 발생시 예외 던짐
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
